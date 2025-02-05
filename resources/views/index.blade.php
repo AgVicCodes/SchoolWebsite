f1c9a915096c15843cc1210409ce0190550e2a17
AGBELEYE VICTOR <65957206+AgVicCodes@users.noreply.github.com>
2025-02-05 18:51:35 +0000

    Modified Homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,93 @@
 @section('title', 'HOME')
 
 @section('content')
-    <h1>HOMEPAGE</h1>
+    <h1>Raising future Acumen Leaders</h1>
     <p>
-        Welcome to ACUMEN COLLEGE
+        Welcome to Acumen College: 
+        Where Knowledge Meets Purpose. 
+        Our mission is to empower you with the skills, 
+        wisdom, and resilience needed to excel in 
+        an ever-evolving world. Explore our programs, 
+        embrace your potential, and embark on a transformative 
+        educational journey with us.
     </p>
+    <button>LEARN MORE</button>
+
+    <h1>WELCOME TO ACUMEN COLLEGE</h1>
+    <p>
+        Welcome to Acumen College, a place where 
+        young minds are nurtured and inspired to become the leaders of tomorrow.
+        We take great pride in creating a secure and dynamic 
+        learning environment that caters to students of all abilities and backgrounds, 
+        from our earliest learners in daycare and nursery to those advancing 
+        through primary and secondary education. We firmly believe that every 
+        child deserves the opportunity to thrive and reach their full potential.
+    </p>
+    <p>
+        Beyond our academic programs, we offer a diverse range of co-curricular 
+        activities, providing students with opportunities to explore their 
+        interests and develop new skills. Whether it's sports, music, theater, 
+        or another passion, Acumen College has something for everyone. 
+        We're here to nurture not only strong minds but also well-rounded individuals.
+        Welcome to a place where the pursuit of knowledge, personal growth, 
+        and diverse talents converge to shape the leaders and innovators of the future.
+    </p>
+    
+    <ul>
+        <li><span>500+ Students</span></li>
+        <li><span>50+ Staff</span></li>
+        <li><span>200+ Subjects and Activities</span></li>
+        <li><span>200+ Facilities</span></li>
+    </ul>
+
+    <h1>WHY ACUMEN COLLEGE?</h1>
+
+    <i><span>people</span></i>
+    <h3>Outstanding Staff</h3>
+    <p>
+        Acumen College is more than a laundry list of 
+        opportunities forged or found, it's  more than an  
+        intellectual environment, more than a 'rigorous education'. 
+    </p>
+
+    <i><span>book</span></i>
+    <h3>Broad Curriculum</h3>
+    <p>
+        Acumen College is more than a laundry list of 
+        opportunities forged or found, it's  more than an  
+        intellectual environment, more than a 'rigorous education'. 
+    </p>
+
+    <i><span>Desktop</span></i>
+    <h3>Great Facilities</h3>
+    <p>
+        Acumen College is more than a laundry list of 
+        opportunities forged or found, it's  more than an  
+        intellectual environment, more than a 'rigorous education'. 
+    </p>
+
+    <h1>POPULAR ACTIVITIES</h1>
+
+    <img src="" alt = "club events">
+    <h4>Clubs and Societies</h4>
+    <p>
+        "At Acumen College, we understand that learning extends far 
+        beyond the classroom. That's why we offer a vibrant array 
+        of clubs and societies designed to spark creativity, 
+        and broaden horizons. Whether your passion is in the arts, 
+        science, service, or sports, our extracurricular 
+        activities provides a platform for you to explore.
+    </p>
+
+    <img src="" alt = "club events">
+    <h4>Clubs and Societies</h4>
+    <p>
+        "At Acumen College, we understand that learning extends far 
+        beyond the classroom. That's why we offer a vibrant array 
+        of clubs and societies designed to spark creativity, 
+        and broaden horizons. Whether your passion is in the arts, 
+        science, service, or sports, our extracurricular 
+        activities provides a platform for you to explore.
+    </p>
+
 @endsection
